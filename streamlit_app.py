63b695b35454236450a45ffda2d3946dbf5b4f28
ylewis-gh <yesmin.lewis@gmail.com>
2025-11-09 01:09:09 -0800

    Update streamlit_app.py
    added column search on

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,8 +13,9 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write ('The name on your Smoothie will be: ', name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
-#st.dataframe(data=my_dataframe, use_container_width=True)
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width=True)
+st.stop()
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingridients: '
